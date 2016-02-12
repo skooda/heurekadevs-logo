5f4e848684be936dfd07e44ca9dd3ad2131f37f1
Pavel Škoda <email@skooda.org>
2016-02-12 17:26:37 +0100

    Add PHP version

---
+++ logo.php
@@ -0,0 +1,20 @@
+
+            <?php echo           
+         "Being a devel".          
+       "ope"        ."r a".       
+      "t".           " Heu".       
+     "re"         ."ka.cz i"      
+    ."s"   ."".   " an"  ." "      
+    ."p"   ."a" ."ss".   "i".      
+    "o".    "n, hon"    ."or"      
+     ." "    ." an"    ."d f"      
+      ."un"          .". Y"        
+        ."ou can join us. ".       
+            "Work at "."He".     
+                         "ure".    
+                           "ka!   
+
+    ╦ ╦┌─┐┬ ┬┬─┐┌─┐┬┌─┌─┐ ┌─┐┌─┐
+    ╠═╣├┤ │ │├┬┘├┤ ├┴┐├─┤ │  ┌─┘
+    ╩ ╩└─┘└─┘┴└─└─┘┴ ┴┴ ┴o└─┘└─┘
+    Programujte s přehledem ";?>
